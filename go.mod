8bf5adb42272e85ffb3982bd6edbcac01b5c4ec0
vijayaragavans <msjvmv@gmail.com>
2025-11-14 22:44:48 +0530

    added basic file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vijayaragavans/secret
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+)
+
+var Lang = "en"
+
+func withTimeout(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a context with a 10-second timeout
+		ctx, cancel := context.WithTimeout(r.Context(), 50*time.Second)
+		defer cancel()
+
+		// Replace the request's context with the new one
+		r = r.WithContext(ctx)
+
+		// Handle the request
+		done := make(chan struct{})
+		go func() {
+			next.ServeHTTP(w, r)
+			close(done)
+		}()
+
+		select {
+		case <-ctx.Done():
+			// If the context is done, return a timeout response
+			if ctx.Err() == context.DeadlineExceeded {
+			}
+		case <-done:
+			// Request completed successfully
+		}
+	})
+}
+
+func main() {
+
+	r := mux.NewRouter()
+
+	//r.HandleFunc("/ismobilevalid", login.IsMobileValid).Methods("POST")
+
+	r.Use(withTimeout)
+
+	c := cors.AllowAll()
+	handler := c.Handler(r)
+
+	log.Fatal(http.ListenAndServe(":8080", handler))
+
+}
